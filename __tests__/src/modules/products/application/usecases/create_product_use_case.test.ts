426dd0adc6ce1b2ebd19f141920e7ee57a2a9f11
fagnercontato <fagner.contato@outlook.com>
2025-02-15 15:29:13 -0300

    implementando o jest

---
+++ __tests__/src/modules/products/application/usecases/create_product_use_case.test.ts
@@ -0,0 +1,10 @@
+describe('CreateProductUseCase', () => {
+    it('test', () => {
+        // given
+        // when
+        const sum = 1+1;
+        
+        // then
+        expect(sum).toEqual(66);
+    });
+});
